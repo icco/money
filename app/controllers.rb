17afe72b1cd4e72e78f19eb6fc9742c4a1e75058
Nat Welch <nat@natwelch.com>
2013-01-14 00:46:48 -0800

    trying to get charts working.

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,7 +1,19 @@
 Money.controllers  do
+  ONE_HOUR = 60*60
+
   layout :main
 
-  get :index do
+  get :index, :cache => true do
     render :index
+  end
+
+  get :'data.csv', :cache => true do
+    expires_in ONE_HOUR
+
+    @accounts = Account.all
+
+    etag "data/accounts-#{Account.maximum(:created_at)}"
+    content_type "text/csv"
+    render :data, :layout => false
   end
 end
