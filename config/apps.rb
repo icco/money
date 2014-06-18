a6a2d6d16c4d6d6c3c963763e1b5f6081e2a3aff
Nat Welch <nat@natwelch.com>
2014-06-18 07:11:55 +0100

    Update apps.rb

--- config/apps.rb
+++ config/apps.rb
@@ -3,7 +3,7 @@
 # override these settings in the subapps as needed.
 #
 Padrino.configure_apps do
-  set :session_secret, ENV['DATABASE_URL'] || '080e5c0c277cc6097ca3f3747e53e05d05d2867a08e82798b0ba0b3d037dcb6e'
+  set :session_secret, ENV['SESSION_SECRET'] || '080e5c0c277e05d05d2867a08e82798b0ba0b3d037dcb6e'
 end
 
 # Mounts the core application for this project
